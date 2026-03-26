361577ca44533506effb7f9ef35206babe4f514d
Thevindu Dharmadasa <thevinduhansaja81@gmail.com>
2026-03-26 14:57:39 +0530

    Updated Discount entity type✅

---
+++ athukorala-backend/inventory-system/src/main/java/com/athukorala/inventory_system/entity/DiscountType.java
@@ -0,0 +1,9 @@
+package com.athukorala.inventory_system.entity;
+
+/**
+ * Public Enum for Role-Based Discount Logic [cite: 406-408]
+ */
+public enum DiscountType {
+    PERCENTAGE,
+    FIXED_AMOUNT
+}
