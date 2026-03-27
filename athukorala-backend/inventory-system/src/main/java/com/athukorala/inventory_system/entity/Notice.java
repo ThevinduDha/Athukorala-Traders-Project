2b8ac807a8408e69eecd7bed71f7dd6baabd8bf1
Thevindu Dharmadasa <thevinduhansaja81@gmail.com>
2026-03-27 21:21:21 +0530

    update Notice.java✅

--- athukorala-backend/inventory-system/src/main/java/com/athukorala/inventory_system/entity/Notice.java
+++ athukorala-backend/inventory-system/src/main/java/com/athukorala/inventory_system/entity/Notice.java
@@ -3,20 +3,27 @@ package com.athukorala.inventory_system.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Entity
-@Table(name = "staff_notices")
 @Data
 public class Notice {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(length = 1000)
     private String message;
 
-    private String targetRole; // e.g., "STAFF" [cite: 956]
+    private String targetRole; // "STAFF" or "CUSTOMER"
+
     private LocalDateTime createdAt;
+
+    private LocalDate startDate;  // THE DAY THE PROMO ACTIVATES
+    private LocalDate expiryDate; // THE DAY THE PROMO EXPIRES
+    private boolean urgent;
+    private boolean active;
 }
