c3fb8f4f61f2b454cfb76a76c21ed7ebde6b5f01
Thevindu Dharmadasa <thevinduhansaja81@gmail.com>
2026-04-13 23:57:29 +0530

    Updated EmailService✅

---
+++ athukorala-backend/inventory-system/src/main/java/com/athukorala/inventory_system/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.athukorala.inventory_system.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendResetEmail(String toEmail, String token) {
+
+        String resetLink = "http://localhost:5173/reset-password?token=" + token;
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("Password Reset Request");
+        message.setText(
+                "Click the link to reset your password:\n\n" + resetLink +
+                        "\n\nThis link expires in 15 minutes."
+        );
+
+        mailSender.send(message);
+    }
+}
